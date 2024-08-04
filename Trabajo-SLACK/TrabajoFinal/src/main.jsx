e2d3fa018dc4195faa4f55251187905d99262d4d
Nico.Alonso <nadiamocse@outlook.es>
2024-08-04 19:11:15 -0300

    Trabajofinal

---
+++ Trabajo-SLACK/TrabajoFinal/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react-router-bootstrap';
+import ReactDOM from 'react-dom/client'; // Importa desde 'react-dom/client'
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Crea un root
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
